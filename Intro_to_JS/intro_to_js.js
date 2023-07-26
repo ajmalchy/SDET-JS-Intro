c0204309abe3ebae6a3015f59e01afa7e8108782
ajmalchy <ajmal.chy.biz@gmail.com>
2023-07-26 16:37:42 -0400

    first commit

---
+++ Intro_to_JS/intro_to_js.js
@@ -0,0 +1,24 @@
+// challenge 1:
+const studentName = 'Ajmal';
+console.log(studentName);
+
+// challenge 2:
+const age = 25;
+console.log(age);
+
+// challenge 3:
+
+let isStudent = true;
+console.log(isStudent);
+
+// challenge 4:
+const favoriteColors = ['gray', 'black', 'white'];
+console.log(favoriteColors); 
+
+// challenge 5:
+const person = {
+    studentName : 'John',
+    age : 27,
+    isStudent : true
+}
+console.log(person);
